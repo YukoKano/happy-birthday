76408b651f0c93efc3ced6cd4a77e8ea23e12c04
YukoKano <halloween_1031@hotmail.co.jp>
2023-08-12 10:30:45 +0900

    誕生日お祝いのリポジトリ作成

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default function App() {
+  function Box({ onBoxChange }) { // 関数渡す時は{}で囲うのが大事っぽい
+    return (
+      <input className="box" type="text" inputMode="numeric" maxLength="1" pattern='\d' onChange={onBoxChange} />
+    );
+  }
 
-export default App;
+  function Boxes() {
+    const [collectText, setIsCollectText] = useState("");
+    const [numbers, setNumbers] = useState(Array(4));
+
+    function Button() {
+      function click() {
+        const collectNumbers = ['2', '2', '4', '0'];
+
+        if (numbers.toString() === collectNumbers.toString()) {
+          setIsFinished(true);
+        } else {
+          setIsCollectText("残念！答えが間違っているよ😢");
+        }
+      }
+      return (
+        <button onClick={click}>答えを送信！</button>
+      )
+    }
+
+    function handleClick(i, value) {
+      const nextNumbers = numbers.slice();
+      nextNumbers[i] = value;
+      setNumbers(nextNumbers);
+      setIsCollectText("");
+    }
+
+    return (
+      <>
+        <div className='boxes'>
+          <Box value={numbers[0]} onBoxChange={(event) => handleClick(0, event.target.value)}/>
+          <Box value={numbers[1]} onBoxChange={(event) => handleClick(1, event.target.value)}/>
+          <Box value={numbers[2]} onBoxChange={(event) => handleClick(2, event.target.value)}/>
+          <Box value={numbers[3]} onBoxChange={(event) => handleClick(3, event.target.value)}/>
+        </div>
+        <Button />
+        <p>{ collectText }</p>
+      </>
+    )
+  }
+
+  const [isFinished, setIsFinished] = useState(false);
+
+  if (!isFinished) {
+    return (
+      <div className='contents'>
+        <label>最終問題の答えをここに入力してね</label>
+        <Boxes />
+      </div>
+    );
+  } else {
+    return (
+      <div className='contents'>
+        <p className='message'>最終問題、大正解！🌻<br />謎解きクリアです〜〜おめでとう！🎊🎉</p>
+        <p className='message'>お誕生日おめでとうのメッセージ</p>
+      </div>
+    )
+  }
+}
